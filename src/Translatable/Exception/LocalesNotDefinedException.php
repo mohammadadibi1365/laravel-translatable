546ae15166c5e1626f601ff254abc0d164cf4e0b
mohammad adibi <mohammadadibi1365@gmail.com>
2022-04-15 13:55:20 +0430

    Update LocalesNotDefinedException.php

--- src/Translatable/Exception/LocalesNotDefinedException.php
+++ src/Translatable/Exception/LocalesNotDefinedException.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Astrotomic\Translatable\Exception;
+namespace Adibi\Translatable\Exception;
 
 class LocalesNotDefinedException extends \Exception
 {
     public static function make(): self
     {
-        return new static('Please make sure you have run `php artisan vendor:publish --provider="Astrotomic\Translatable\TranslatableServiceProvider"` and that the locales configuration is defined.');
+        return new static('Please make sure you have run `php artisan vendor:publish --provider="Adibi\Translatable\TranslatableServiceProvider"` and that the locales configuration is defined.');
     }
 }
