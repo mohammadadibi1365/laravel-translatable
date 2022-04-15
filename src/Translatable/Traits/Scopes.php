8bcb65d93da0df39ca84832723a569902f5dae33
mohammad adibi <mohammadadibi1365@gmail.com>
2022-04-15 13:54:48 +0430

    Update Scopes.php

--- src/Translatable/Traits/Scopes.php
+++ src/Translatable/Traits/Scopes.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Astrotomic\Translatable\Traits;
+namespace Adibi\Translatable\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\Relation;
